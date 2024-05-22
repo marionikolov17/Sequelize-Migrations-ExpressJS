2bc3486def8ea45146ff9178548026588841c090
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-22 19:24:18 +0300

    Update user.ts

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,23 +1,15 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+import { Model } from "sequelize";
+import connection from "./../connection";
+
+class User extends Model {
+
+}
+
+User.init({
+
+}, {
+  tableName: "athleans-users",
+  sequelize: connection
+});
+
+export default User;

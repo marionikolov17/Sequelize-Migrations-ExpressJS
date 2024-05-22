b34c4abc52f30aa1cf6fb671bf802bea7c8a12d9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-22 20:18:16 +0300

    Update 20240522161605-create-user.ts

--- src/database/migrations/20240522161605-create-user.ts
+++ src/database/migrations/20240522161605-create-user.ts
@@ -12,8 +12,57 @@ const functions = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
-        type: Sequelize.STRING
+      first_name: {
+        type: Sequelize.STRING(45),
+        allowNull: true,
+        defaultValue: null
+      },
+      last_name: {
+        type: Sequelize.STRING(45),
+        allowNull: true,
+        defaultValue: null
+      },
+      username: {
+        type: Sequelize.STRING(45),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      profile_picture_url: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+        defaultValue: true
+      },
+      country: {
+        type: Sequelize.STRING(45),
+        allowNull: true,
+        defaultValue: null
+      },
+      languages: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      phone_number: {
+        type: Sequelize.STRING(25),
+        allowNull: true,
+        defaultValue: null
+      },
+      user_role: {
+        type: Sequelize.TINYINT({ length: 1 }),
+        allowNull: false,
+        defaultValue: -1
+      },
+      visible: {
+        type: Sequelize.TINYINT.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1
+      },
+      date_created: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Date.now()
       },
       createdAt: {
         allowNull: false,

2c90221712315f79f3a106697b75f109be4334ba
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-22 19:31:33 +0300

    Update user.ts

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -2,7 +2,22 @@ import { Model } from "sequelize";
 import connection from "./../connection";
 
 class User extends Model {
+  public id!: number;
+  public first_name!: string;
+  public last_name!: string;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public profile_picture_url!: string;
+  public country!: string;
+  public languages!: string;
+  public phone_number!: string;
+  public user_role!: number;
+  public visible!: number;
+  public date_created!: Date;
 
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 User.init({

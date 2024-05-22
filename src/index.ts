6ee9f18bb632b4ff14503c261fccf3f37d8954e4
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-22 20:35:34 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
 import express from "express";
 
+import User from "./database/models/user";
+
 const app = express();
 
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.status(200).json({
+app.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const createdUser = await User.create({
+      first_name: "KING USER",
+      last_name: "KING",
+      username: "KINGUSER",
+      email: "king@gmail.com",
+      password: "1234",
+      profile_picture_url: "/url/to/photo",
+      languages: "1,2,3",
+      user_role: 9,
+      visible: 0
+    });
+
+    res.status(201).json({
         status: "success",
         data: {
-            message: "Server is working!"
+            message: "Successfully created user!",
+            user: createdUser
         }
-    });
+    })
+  } catch (err) {
+    res.status(500).json({
+        status: "error",
+        data: {
+            message: err.message
+        }
+    })
+  }
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });

08d5a4386f2dc666057621f9ad3c95f516613da7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-05-22 20:16:04 +0300

    Update user.ts

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,4 +1,4 @@
-import { Model } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import connection from "./../connection";
 
 class User extends Model {
@@ -21,7 +21,64 @@ class User extends Model {
 }
 
 User.init({
-
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true
+  },
+  first_name: {
+    type: DataTypes.STRING(45),
+    allowNull: true,
+    defaultValue: null
+  },
+  last_name: {
+    type: DataTypes.STRING(45),
+    allowNull: true,
+    defaultValue: null
+  },
+  username: {
+    type: DataTypes.STRING(45),
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  profile_picture_url: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+    defaultValue: true
+  },
+  country: {
+    type: DataTypes.STRING(45),
+    allowNull: true,
+    defaultValue: null
+  },
+  languages: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  phone_number: {
+    type: DataTypes.STRING(25),
+    allowNull: true,
+    defaultValue: null
+  },
+  user_role: {
+    type: DataTypes.TINYINT({ length: 1 }),
+    allowNull: false,
+    defaultValue: -1
+  },
+  visible: {
+    type: DataTypes.TINYINT.UNSIGNED,
+    allowNull: false,
+    defaultValue: 1
+  },
+  date_created: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Date.now()
+  }
 }, {
   tableName: "athleans-users",
   sequelize: connection
